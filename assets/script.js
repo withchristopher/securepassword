9d75ccbe621eae65d29ea1bd3b85681e240c41a5
Chris <chris.reviresco@gmail.com>
2020-06-07 17:51:26 +0200

    added all possible char's

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,23 @@
 // Assignment code here
 
+// all available characters
+const key_strings = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  number: "0123456789",
+  symbol: "*;<>()[]{}#$?!^|",
+};
+
+// function to generate password, no arguments to pass
 function generatePassword() {
+  // first prompt to ask user to enter length between 8 and 128
   numChar = parseInt(
     prompt(
       "Welcome. You will now be asked a series of prompts. Please enter the length between 8 and 128 characters"
     )
   );
 
+  // conditional for Not a Number, <8 and >128 characters
   if (isNaN(numChar) || numChar < 8 || numChar > 128) {
     numChar = parseInt;
     prompt("Oops! Please enter a password between 8 and 128 characters");
@@ -16,6 +27,11 @@ function generatePassword() {
     numCase = confirm("Would you like numbers in your password?");
     uppCase = confirm("Would you like special case in your password?");
   }
+
+  // randrom number generator
+  var passwordString = Math.random().toString(36).slice(-8);
+
+  console.log(passwordString);
 }
 
 // Get references to the #generate element
